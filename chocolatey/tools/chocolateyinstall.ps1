6989400f618a86aadf306384f9033ab3736fa7f8
antybrowser.com <support@antybrowser.com>
2026-09-07 18:07:31 +0800

    chore: update all manifests with real SHA256 checksums from cli-v1.0.0 release

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 
 $url = 'https://github.com/antybrowser/antybrowser-cli/releases/download/cli-v1.0.0/antybrowser-windows-amd64.zip'
-$checksum = 'PLACEHOLDER_SHA256'
+$checksum = '656a6401b20ba7541d90f986942816c703e6bb98d966469fd7a6f45e6b36525b'
 $checksumType = 'sha256'
 
 Install-ChocolateyZipPackage -PackageName 'antybrowser-cli' -Url $url -UnzipLocation "$env:toolsDirectory" -Checksum $checksum -ChecksumType $checksumType
--- homebrew/antybrowser-cli.rb
+++ homebrew/antybrowser-cli.rb
@@ -7,20 +7,20 @@ class AntybrowserCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/antybrowser/antybrowser-cli/releases/download/cli-v1.0.0/antybrowser-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "4a55d93388be5227f0f596f745057ef8418a7de70242d007017f1c7cd56dc889"
     else
       url "https://github.com/antybrowser/antybrowser-cli/releases/download/cli-v1.0.0/antybrowser-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "daa594ff4514992e456a6bdf1a04b37c21bbce835aa854bf1f8f19f34fa7e861"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/antybrowser/antybrowser-cli/releases/download/cli-v1.0.0/antybrowser-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "d1d16f35473b08261f4509d942dbaa451bbbb9cd6a2cac2f5fab92b46ce98445"
     else
       url "https://github.com/antybrowser/antybrowser-cli/releases/download/cli-v1.0.0/antybrowser-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "afaa12f4ad4c7fe9d2c8302818db11e26b3a0e66a82937e3785403a05e808a8b"
     end
   end
 
